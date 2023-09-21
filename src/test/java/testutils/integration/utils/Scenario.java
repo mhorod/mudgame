8e724a332eabb0996b11801f549175a5e36c23fe
Michał Pajda <michal.pajda2002@gmail.com>
2023-09-22 01:00:30 +0200

    Added animation tests

--- src/test/java/testutils/integration/utils/Scenario.java
+++ src/test/java/testutils/integration/utils/Scenario.java
@@ -11,9 +11,11 @@ import mudgame.server.MudServerCore;
 import mudgame.server.state.ServerState;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.Consumer;
 
 import static java.util.stream.Collectors.toMap;
 
@@ -23,15 +25,27 @@ public class Scenario {
         @Override
         public void receive(EventOccurrence eventOccurrence) {
             for (PlayerID player : eventOccurrence.recipients()) {
+                if (beforeApply.containsKey(player))
+                    beforeApply.get(player).accept(eventOccurrence.event());
                 clientCores.get(player).receive(eventOccurrence.event());
                 receivedEvents.get(player).add(eventOccurrence.event());
             }
         }
     }
 
+    public void setBeforeApply(PlayerID id, Consumer<Event> f) {
+        beforeApply.put(id, f);
+    }
+
+    private final HashMap<PlayerID, Consumer<Event>> beforeApply = new HashMap<>();
+
     private final MudServerCore serverCore;
     private final Map<PlayerID, MudClientCore> clientCores;
-    Map<PlayerID, List<Event>> receivedEvents;
+    private final Map<PlayerID, List<Event>> receivedEvents;
+
+    public List<Event> getReceivedEvents(PlayerID id) {
+        return receivedEvents.get(id);
+    }
 
     public Scenario(ServerState initialState) {
         EventSender sender = new EventSender();
@@ -52,8 +66,13 @@ public class Scenario {
                 ));
     }
 
-    public MudServerCore serverCore() { return serverCore; }
-    public MudClientCore clientCore(PlayerID playerID) { return clientCores.get(playerID); }
+    public MudServerCore serverCore() {
+        return serverCore;
+    }
+
+    public MudClientCore clientCore(PlayerID playerID) {
+        return clientCores.get(playerID);
+    }
 
     public Scenario act(PlayerID player, Action... actions) {
         for (Action a : actions)
